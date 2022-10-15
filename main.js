e47cbb1cb5c0210910763aad395b6d1d3334e4bc
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-10-15 10:20:22 +0530

    Using `fetch()` and Adding Copy Button

--- main.js
+++ main.js
@@ -1,61 +1,73 @@
-const button = document.getElementById('button');
-const joke = document.getElementById('joke');
-const reset = document.getElementById('reset');
-const delivery = document.getElementById('delivery')
+const generate = document.getElementById("generate");
+const joke = document.getElementById("joke");
+const reset = document.getElementById("reset");
+const delivery = document.getElementById("delivery");
+const copyBtn = document.querySelector("#copy_btn");
 
 /* API Base URL*/
-const baseURL = "https://sv443.net/jokeapi/v2";
+const baseURL = "https://sv443.net/jokeapi/v2/joke/Any?";
 
 /* Color Array: to change the backgroundColor of the JOKE*/
-const color = ["lightgreen", "lightblue", "gray", "lightgrey", "skyblue", "wheat", "white", "lightcyan", "antiquewhite", "azure", "lightyellow", "mintcream", "springgreen", "snow", "seashell"]
-
+const color = [
+  "lightgreen",
+  "lightblue",
+  "gray",
+  "lightgrey",
+  "skyblue",
+  "wheat",
+  "white",
+  "lightcyan",
+  "antiquewhite",
+  "azure",
+  "lightyellow",
+  "mintcream",
+  "springgreen",
+  "snow",
+  "seashell",
+];
 
 /* Generate Buttton click event */
-button.addEventListener('click', () => {
-	/* Setting Joke Color to Black*/
-    joke.style.color = "black";
+generate.addEventListener("click", () => {
+  /* Setting Joke Color to Black*/
+  joke.style.color = "black";
 
-	/* Requesting for Joke*/
-    const Http = new XMLHttpRequest();
-    Http.open("GET", baseURL + "/joke/Any?");
+  fetch(baseURL)
+    .then((res) => res.json())
+    .then((randomJoke) => {
+      console.log(randomJoke);
+      if (randomJoke.type === "single") {
+        // If type == "single", the joke only has the "joke" property
+        joke.innerHTML = randomJoke.joke;
+        delivery.innerHTML = "";
+        delivery.style.display = "none";
+      } else {
+        // If type == "single", the joke only has the "joke" property
+        joke.innerHTML = randomJoke.setup;
+        delivery.style.display = "block";
+        delivery.innerHTML = randomJoke.delivery;
+        delivery.style.backgroundColor = "yellow";
+      }
 
-    Http.onreadystatechange = function() {
-        if (Http.readyState == 4 && Http.status < 300) // readyState 4 means request has finished + we only want to parse the joke if the request was successful (status code lower than 300)
-        {
-            const randomJoke = JSON.parse(Http.responseText);
-            console.log(randomJoke);
-
-            if (randomJoke.type == "single") {
-                // If type == "single", the joke only has the "joke" property
-                joke.innerHTML = randomJoke.joke;
-                delivery.innerHTML = "";
-                delivery.style.backgroundColor = "black";
-            } else {
-                // If type == "single", the joke only has the "joke" property
-                joke.innerHTML = randomJoke.setup;
-                delivery.innerHTML = randomJoke.delivery;
-                delivery.style.backgroundColor = "yellow";
-            }
-
-
-            joke.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
-
-
-
-        } else if (Http.readyState == 4) {
-            alert("Error while requesting joke.\n\nStatus code: " + Http.status + "\nServer response: " + Http.responseText);
-        }
-    };
-
-    Http.send();
+      console.log(copyBtn);
+      copyBtn.style.display = "flex";
+      joke.style.backgroundColor =
+        color[Math.floor(Math.random() * color.length)];
+    });
 });
 
 /* Resetting the Page onClick */
-reset.addEventListener('click', () => {
-    joke.innerHTML = "Click Generate Button";
-    joke.style.backgroundColor = "black";
-    joke.style.color = "white";
-    delivery.innerHTML = "";
-    delivery.style.backgroundColor = "black";
+reset.addEventListener("click", () => {
+  joke.innerHTML = "Click Generate Button";
+  joke.style.backgroundColor = "black";
+  joke.style.color = "white";
+  delivery.style.backgroundColor = "black";
+  copyBtn.style.display = "none";
+  elivery.style.display = "none";
+});
 
-})
+copyBtn.onclick = () => {
+  var exportJoke = joke.textContent + "\n\n" + delivery.textContent;
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(exportJoke);
+};
